5fb4711f3029fbad23bb375065e583588b787604
19746 <mattiabraga2006@gmail.com>
2024-07-12 14:41:43 +0200

    Done the backbone of the API, done the new_transaction request

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # StellaNova
+
+## Description
+
+StellaNova is a blockchain for the StellaNova coin. It is a simple blockchain that allows for the creation of new blocks, the mining of new blocks, and the validation of blocks. The blockchain is written in Python and uses the Flask web framework to create a simple web interface for interacting with the blockchain.
+
+## Author
+
+[Neetre](https://github.com/Neetre)
--- bin/blockchain.py
+++ bin/blockchain.py
@@ -1,8 +1,10 @@
 import hashlib
 import json
-
+from textwrap import dedent
 from time import time
 from uuid import uuid4
+
+from flask import Flask, jsonify, request
 
 class BlockChain(object):
     def __init__(self) -> None:
@@ -51,7 +53,7 @@ class BlockChain(object):
         return self.last_block['index'] + 1
     
     @property
-    def last_bloc(self):
+    def last_block(self):
         return self.chain[-1]
     
     @staticmethod
@@ -65,3 +67,69 @@ class BlockChain(object):
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+    
+    def proof_or_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+        
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+            
+        return proof
+    
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+        guess = f"{last_proof}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"  # more zero == more difficult
+    
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instatiate the Blockchain
+blockchain = BlockChain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    response = {'message': f'Transaction will be added to BLock {index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain' : blockchain.chain,
+        'length' : len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
